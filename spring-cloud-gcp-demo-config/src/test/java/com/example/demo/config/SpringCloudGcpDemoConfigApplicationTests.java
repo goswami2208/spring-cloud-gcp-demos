3aa5d8098d2b4c3eab9da489bce495a584e6a2e6
goswami2208 <goswami.joy@gmail.com>
2024-03-16 09:34:08 -0400

    Initial commit

---
+++ spring-cloud-gcp-demo-config/src/test/java/com/example/demo/config/SpringCloudGcpDemoConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGcpDemoConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
