3b766a87f106f4e361d3e54553a4aed8c6dc34c4
Joy Goswami <joy.goswami@labor.ny.gov>
2024-03-21 10:39:51 -0400

    Initial Commit of spring-cloud-gcp-demo-secretmanager

---
+++ spring-cloud-gcp-demo-secretmanager/src/main/java/com/example/demo/secretmanager/SpringCloudGcpDemoSecretmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.secretmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudGcpDemoSecretmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudGcpDemoSecretmanagerApplication.class, args);
+    }
+
+}
---
+++ spring-cloud-gcp-demo-secretmanager/src/test/java/com/example/demo/secretmanager/SpringCloudGcpDemoSecretmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo.secretmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudGcpDemoSecretmanagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
